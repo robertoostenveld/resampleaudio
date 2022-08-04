037bb4b797c5e5ff70a08f257e607df082ec0765
Robert Oostenveld <r.oostenveld@gmail.com>
2022-08-04 12:47:15 +0200

    made sampling rates configurable on command line This required reordering the setup of stage 1, 2, and 3.

--- resampleaudio.c
+++ resampleaudio.c
@@ -13,14 +13,8 @@
 
 #define SAMPLE_TYPE               paFloat32
 #define CHANNEL_COUNT             (1)
-#define INPUT_RATE                (8000)
-#define OUTPUT_RATE               (48000)
-#define BLOCKSIZE                 (0.01)  // in seconds
-#define INPUT_BLOCKSIZE           (BLOCKSIZE*INPUT_RATE)
-#define OUTPUT_BLOCKSIZE          (BLOCKSIZE*OUTPUT_RATE)
-#define BUFFER                    (2.0)   // in seconds
-#define INPUT_BUFFER              (BUFFER*INPUT_RATE)
-#define OUTPUT_BUFFER             (BUFFER*OUTPUT_RATE)
+#define BLOCKSIZE                 (0.01) // in seconds
+#define BUFFER                    (2.00) // in seconds
 
 typedef struct {
 				float *data;
@@ -33,11 +27,11 @@ SRC_STATE* state = NULL;
 SRC_DATA data;
 
 short enable_resample = 0, enable_update = 0;
-float input_rate = 8000, output_rate = 48000;
-float blocksize = 0.01, buffer = 2.0; // in seconds
-int input_buffer = 800, output_buffer = 4800;
-float ratio = 6;
+float input_rate, output_rate;
+int input_blocksize, output_blocksize, input_buffer, output_buffer;
+float ratio;
 
+/*******************************************************************************************************/
 int resample_buffers(void)
 {
 				data.src_ratio      = ratio;
@@ -45,7 +39,7 @@ int resample_buffers(void)
 				data.data_in        = inputData.data;
 				data.input_frames   = inputData.frames;
 				data.data_out       = outputData.data + outputData.frames * CHANNEL_COUNT * sizeof(float);
-				data.output_frames  = OUTPUT_BUFFER - outputData.frames;
+				data.output_frames  = output_buffer - outputData.frames;
 
 				int err = src_process (state, &data);
 				if (err)
@@ -69,26 +63,26 @@ int resample_buffers(void)
 /*******************************************************************************************************/
 int update_ratio(void)
 {
-        float nominal = (float)OUTPUT_RATE/INPUT_RATE;
-        float current = nominal + (0.5*OUTPUT_BUFFER - outputData.frames) / INPUT_BLOCKSIZE;
+				float nominal = (float)output_rate/input_rate;
+				float estimate = nominal + (0.5*output_buffer - outputData.frames) / input_blocksize;
 
-        /* do not change the ratio by too much */
-        current = min(current, 1.1*nominal);
-        current = max(current, 0.9*nominal);
+				/* do not change the ratio by too much */
+				estimate = min(estimate, 1.1*nominal);
+				estimate = max(estimate, 0.9*nominal);
 
-        /* allow some variation around the target buffer size */
-        float lower = (0.49*OUTPUT_BUFFER);
-        float upper = (0.51*OUTPUT_BUFFER);
+				/* allow some variation around the target buffer size */
+				float lower = (0.49*output_buffer);
+				float upper = (0.51*output_buffer);
 
-        if (outputData.frames<lower || outputData.frames>upper)
-                /* increase or decrease the ratio to the value that appears to be needed */
-                ratio = smooth(ratio, current, 0.001);
-        else
-                /* change the ratio towards the nominal value */
-                ratio = smooth(ratio, nominal, 0.1);
-        
-        printf("%0.f\t%lu\t%0.f\t%f\t%f\n", lower, outputData.frames, upper, current, ratio);
-        
+				if (outputData.frames<lower || outputData.frames>upper)
+								/* increase or decrease the ratio to the value that appears to be needed */
+								ratio = smooth(ratio, estimate, 0.001);
+				else
+								/* change the ratio towards the nominal value */
+								ratio = smooth(ratio, nominal, 0.1);
+
+				printf("%0.f\t%lu\t%0.f\t%f\t%f\n", lower, outputData.frames, upper, estimate, ratio);
+
 				return 0;
 }
 /*******************************************************************************************************/
@@ -103,7 +97,7 @@ static int inputCallback( const void *input,
 				dataBuffer_t *inputData = (dataBuffer_t *)userData;
 
 				for (int i=0; i<frameCount; i++) {
-								if (inputData->frames<INPUT_BUFFER)
+								if (inputData->frames<input_buffer)
 								{
 												for (int j=0; j<CHANNEL_COUNT; j++)
 																inputData->data[inputData->frames+j] = dat[i*CHANNEL_COUNT+j];
@@ -111,11 +105,11 @@ static int inputCallback( const void *input,
 								}
 				}
 
-        if (enable_resample)
+				if (enable_resample)
 								resample_buffers();
-        if (enable_update)
-                update_ratio();
-              
+				if (enable_update)
+								update_ratio();
+
 				return 0;
 }
 
@@ -158,57 +152,16 @@ static int outputCallback( const void *input,
 /*******************************************************************************************************/
 int main(int argc, char *argv[]) {
 				char *line = NULL;
-        size_t linecap = 0;
+				size_t linecap = 0;
 
 				PaStream *inputStream, *outputStream;
 				PaStreamParameters inputParameters, outputParameters;
 				PaError err = paNoError;
-				int numDevices, defaultDisplayed;
+				int numDevices;
 				const PaDeviceInfo *deviceInfo;
 
-				/* STAGE 1: Initialize the inputData and outputData for use by the callbacks. */
-				inputData.frames = 0;
-				inputData.data = NULL;
-				if ((inputData.data = malloc(INPUT_BUFFER*CHANNEL_COUNT*sizeof(float))) == NULL)
-								goto error1;
-				else
-								bzero(inputData.data, INPUT_BUFFER*CHANNEL_COUNT*sizeof(float));
-				outputData.frames = 0;
-				outputData.data = NULL;
-				if ((outputData.data = malloc(OUTPUT_BUFFER*CHANNEL_COUNT*sizeof(float))) == NULL)
-								goto error1;
-				else
-								bzero(outputData.data, OUTPUT_BUFFER*CHANNEL_COUNT*sizeof(float));
+				/* STAGE 1: Initialize the audio input and output. */
 
-				/* STAGE 2: Initialize the resampling. */
-				printf("Setting up %s rate converter with %s\n",
-				       src_get_name (SRC_SINC_MEDIUM_QUALITY),
-				       src_get_description (SRC_SINC_MEDIUM_QUALITY));
-
-				state = src_new (SRC_SINC_MEDIUM_QUALITY, CHANNEL_COUNT, &err);
-				if (state == NULL)
-				{
-								printf("ERROR: src_new returned 0x%x\n", err );
-								printf("ERROR message: %s\n", src_strerror(err));
-								goto error2;
-				}
-
-				/* specify the input and output buffers for the sample rate conversion */
-				data.data_in = NULL;
-				data.data_out = NULL;
-
-				ratio = ((float)OUTPUT_RATE) / ((float)INPUT_RATE);
-
-				printf("ratio = %f\n", ratio);
-				err = src_set_ratio (state, ratio);
-				if (err)
-				{
-								printf("ERROR: src_set_ratio returned 0x%x\n", err );
-								printf("ERROR message: %s\n", src_strerror(err));
-								goto error2;
-				}
-
-				/* STAGE 3: Initialize the audio input and output. */
 				inputParameters.device = paNoDevice;
 				outputParameters.device = paNoDevice;
 
@@ -217,7 +170,7 @@ int main(int argc, char *argv[]) {
 				{
 								printf("ERROR: Pa_Initialize returned 0x%x\n", err );
 								printf("ERROR message: %s\n", Pa_GetErrorText( err ) );
-								goto error3;
+								goto error1;
 				}
 
 				printf("PortAudio version: 0x%08X\n", Pa_GetVersion());
@@ -229,7 +182,7 @@ int main(int argc, char *argv[]) {
 				{
 								printf("ERROR: Cannot initialize PortAudio.\n");
 								printf("ERROR message: %s\n", Pa_GetErrorText( err ) );
-								goto error3;
+								goto error1;
 				}
 
 				numDevices = Pa_GetDeviceCount();
@@ -238,7 +191,7 @@ int main(int argc, char *argv[]) {
 								printf("ERROR: Pa_GetDeviceCount returned 0x%x\n", numDevices );
 								printf("ERROR message: %s\n", Pa_GetErrorText( err ) );
 								err = numDevices;
-								goto error3;
+								goto error1;
 				}
 
 				printf("Number of devices = %d\n", numDevices );
@@ -253,21 +206,26 @@ int main(int argc, char *argv[]) {
 				}
 
 				printf("Select input device: ");
-        getline(&line, &linecap, stdin);
+				getline(&line, &linecap, stdin);
 
 				inputParameters.device = atoi(line);
 				inputParameters.channelCount = CHANNEL_COUNT;
 				inputParameters.sampleFormat = SAMPLE_TYPE;
 				inputParameters.suggestedLatency = Pa_GetDeviceInfo( inputParameters.device )->defaultLowInputLatency;
 				inputParameters.hostApiSpecificStreamInfo = NULL;
-				printf("Initialized input device %d\n", inputParameters.device);
+
+				printf("Give input sampling rate: ");
+				getline(&line, &linecap, stdin);
+				input_rate = atoi(line);
+				input_buffer = BUFFER * input_rate;
+				input_blocksize = BLOCKSIZE * input_rate;
 
 				err = Pa_OpenStream(
 								&inputStream,
 								&inputParameters,
 								NULL,
-								INPUT_RATE,
-								INPUT_BLOCKSIZE,
+								input_rate,
+								input_blocksize,
 								0,
 								inputCallback,
 								&inputData );
@@ -275,28 +233,33 @@ int main(int argc, char *argv[]) {
 				{
 								printf("ERROR: Cannot open input stream.\n");
 								printf("ERROR message: %s\n", Pa_GetErrorText( err ) );
-								goto error3;
+								goto error1;
 				}
 				else {
 								printf("Opened input stream.\n");
 				}
 
 				printf("Select output device: ");
-        getline(&line, &linecap, stdin);
+				getline(&line, &linecap, stdin);
 
 				outputParameters.device = atol(line);
 				outputParameters.channelCount = CHANNEL_COUNT;
 				outputParameters.sampleFormat = SAMPLE_TYPE;
 				outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
 				outputParameters.hostApiSpecificStreamInfo = NULL;
-				printf("Initialized output device %d\n", outputParameters.device);
+
+				printf("Give output sampling rate: ");
+				getline(&line, &linecap, stdin);
+				output_rate = atoi(line);
+				output_buffer = BUFFER * output_rate;
+				output_blocksize = BLOCKSIZE * output_rate;
 
 				err = Pa_OpenStream(
 								&outputStream,
 								NULL,
 								&outputParameters,
-								OUTPUT_RATE,
-								OUTPUT_BLOCKSIZE,
+								output_rate,
+								output_blocksize,
 								paClipOff,
 								outputCallback,
 								&outputData );
@@ -304,13 +267,54 @@ int main(int argc, char *argv[]) {
 				{
 								printf("ERROR: Cannot open output stream.\n");
 								printf("ERROR message: %s\n", Pa_GetErrorText( err ) );
-								goto error3;
+								goto error1;
 				}
 				else {
 								printf("Opened output stream.\n");
 				}
 
-				/************************************************************/
+				/* STAGE 2: Initialize the inputData and outputData for use by the callbacks. */
+
+				inputData.frames = 0;
+				inputData.data = NULL;
+				if ((inputData.data = malloc(input_buffer*CHANNEL_COUNT*sizeof(float))) == NULL)
+								goto error2;
+				else
+								bzero(inputData.data, input_buffer*CHANNEL_COUNT*sizeof(float));
+
+				outputData.frames = 0;
+				outputData.data = NULL;
+				if ((outputData.data = malloc(output_buffer*CHANNEL_COUNT*sizeof(float))) == NULL)
+								goto error2;
+				else
+								bzero(outputData.data, output_buffer*CHANNEL_COUNT*sizeof(float));
+
+				/* STAGE 3: Initialize the resampling. */
+
+				ratio = output_rate / input_rate;
+				printf("Initial ratio = %f\n", ratio);
+
+				printf("Setting up %s rate converter with %s\n",
+				       src_get_name (SRC_SINC_MEDIUM_QUALITY),
+				       src_get_description (SRC_SINC_MEDIUM_QUALITY));
+
+				state = src_new (SRC_SINC_MEDIUM_QUALITY, CHANNEL_COUNT, &err);
+				if (state == NULL)
+				{
+								printf("ERROR: src_new returned 0x%x\n", err );
+								printf("ERROR message: %s\n", src_strerror(err));
+								goto error3;
+				}
+
+				err = src_set_ratio (state, ratio);
+				if (err)
+				{
+								printf("ERROR: src_set_ratio returned 0x%x\n", err );
+								printf("ERROR message: %s\n", src_strerror(err));
+								goto error3;
+				}
+
+				/* STAGE 4: Start the streams. */
 
 				err = Pa_StartStream( outputStream );
 				if( err != paNoError )
@@ -328,61 +332,44 @@ int main(int argc, char *argv[]) {
 								goto error3;
 				}
 
-				/* Sleep for some time to fill the buffer. */
 				printf("Filling buffer.\n");
 
-        Pa_Sleep(1000);
+				/* Wait one second to fill the input BUFFER to 50% */
+				Pa_Sleep(1000);
 				enable_resample = 1;
 
-        Pa_Sleep(1000);
+				/* Wait one second to enable the updating of the resampling ratio */
+				Pa_Sleep(1000);
 				enable_update = 1;
 
 				while (1)
-				{
 								Pa_Sleep(1000);
-        }
 
-								printf("------------------------------\n");
-								printf("inputData.frames        = %ld\n", inputData.frames);
-								printf("outputData.frames       = %ld\n", outputData.frames);
-								printf("data.src_ratio          = %f\n", data.src_ratio);
-								printf("data.input_frames_used  = %ld\n", data.input_frames_used);
-								printf("data.output_frames_gen  = %ld\n", data.output_frames_gen);
-								printf("input stream CPU load   = %.2f %%\n", 100*Pa_GetStreamCpuLoad(inputStream));
-								printf("output stream CPU load  = %.2f %%\n", 100*Pa_GetStreamCpuLoad(outputStream));
+				printf("Running.\n");
 
 				err = Pa_StopStream( outputStream );
 				if( err != paNoError ) goto error3;
 				err = Pa_CloseStream( outputStream );
 				if( err != paNoError ) goto error3;
 
-				Pa_Terminate();
-				printf("Test finished.\n");
-				printf("inputData.frames     = %lu\n", inputData.frames);
-				printf("outputData.frames    = %lu\n", outputData.frames);
-
-				free(inputData.data);
-				free(outputData.data);
-        free(line);
-
-				return 0;
-
 error3:
-				Pa_Terminate();
-
-error2:
 				if (state)
 								src_delete (state);
 
-error1:
+error2:
 				if (inputData.data)
 								free(inputData.data);
 				if (outputData.data)
 								free(outputData.data);
-        if (line)
-                free(line);
+				if (line)
+								free(line);
 
-				printf("Error number: %d\n", err );
+error1:
+				Pa_Terminate();
 
+				if (err)
+								printf("Error number: %d\n", err );
+				else
+								printf("Finished.");
 				return err;
 }
